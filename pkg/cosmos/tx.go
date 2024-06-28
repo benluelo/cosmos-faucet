a50386b996c5bc2620f438ef9ba36f7029f35f0c
benluelo <git@luelo.dev>
2024-06-28 10:41:24 -0400

    fix: don't drop txs on account sequence mismatch

--- pkg/cosmos/tx.go
+++ pkg/cosmos/tx.go
@@ -81,75 +81,87 @@ func (handler *TxHandler) Receive(ctx actor.Context) {
 		handler.cosmosClient = ctx.Spawn(handler.cosmosClientProps)
 
 	case *message.MakeTx:
-		if time.Now().After(msg.Deadline) {
-			log.Warn().Msg("😞 Deadline exceeded, ignore transaction.")
-			break
-		}
-
-		unsignedTx, err := handler.BuildUnsignedTx(msg.Msgs, msg.Memo, msg.GasLimit, msg.FeeAmount)
-		if err != nil {
-			log.Panic().Err(err).Msg("❌ Could not build transaction.")
-		}
-
-		accountResp, err := ctx.RequestFuture(
-			handler.cosmosClient,
-			&message.GetAccount{Deadline: msg.Deadline, Address: handler.address},
-			time.Until(msg.Deadline),
-		).Result()
-		if err != nil {
-			log.Panic().Err(err).Msg("❌ Could not get account information.")
-		}
-
-		var account *auth.BaseAccount
-		switch resp := accountResp.(type) {
-		case *message.GetAccountResponse:
-			account = resp.Account
-		default:
-			log.Panic().Err(fmt.Errorf("wrong response message")).Msg("❌ Could not get account information.")
-		}
-
-		signedTx, err := handler.SignTx(
-			unsignedTx, authsigning.SignerData{
-				ChainID:       handler.chainID,
-				AccountNumber: account.GetAccountNumber(),
-				Sequence:      account.GetSequence(),
-			},
-		)
-		if err != nil {
-			log.Panic().Err(err).Msg("❌ Could not sign transaction.")
-		}
-
-		tx, err := handler.EncodeTx(signedTx)
-		if err != nil {
-			log.Panic().Err(err).Msg("❌ Could not encode transaction.")
-		}
-
-		txResp, err := ctx.RequestFuture(
-			handler.cosmosClient,
-			&message.BroadcastTx{Deadline: msg.Deadline, Tx: tx},
-			time.Until(msg.Deadline),
-		).Result()
-		if err != nil {
-			log.Panic().Err(err).Msg("❌ Could not broadcast transaction.")
-		}
-
-		switch resp := txResp.(type) {
-		case *message.BroadcastTxResponse:
-			ctx.Send(msg.TxSubscriber, txResp)
-			if resp.TxResponse.Code != 0 {
-				log.Warn().
-					Int("messageCount", len(msg.Msgs)).
-					Interface("tx", resp.TxResponse).
-					Msg("😞 Transaction submitted with non 0 code")
-			} else {
-				log.Info().
-					Int("messageCount", len(msg.Msgs)).
-					Str("txHash", resp.TxResponse.TxHash).
-					Uint32("txCode", resp.TxResponse.Code).
-					Msg("🚀 Successfully submit transaction")
+		var attemptNum = 1
+		for {
+			if time.Now().After(msg.Deadline) {
+				log.Warn().Msg("😞 Deadline exceeded, ignore transaction.")
+				break
 			}
-		default:
-			log.Panic().Err(fmt.Errorf("wrong response message")).Msg("❌ Could not broadcast transaction.")
+
+			unsignedTx, err := handler.BuildUnsignedTx(msg.Msgs, msg.Memo, msg.GasLimit, msg.FeeAmount)
+			if err != nil {
+				log.Panic().Err(err).Msg("❌ Could not build transaction.")
+			}
+
+			accountResp, err := ctx.RequestFuture(
+				handler.cosmosClient,
+				&message.GetAccount{Deadline: msg.Deadline, Address: handler.address},
+				time.Until(msg.Deadline),
+			).Result()
+			if err != nil {
+				log.Panic().Err(err).Msg("❌ Could not get account information.")
+			}
+
+			var account *auth.BaseAccount
+			switch resp := accountResp.(type) {
+			case *message.GetAccountResponse:
+				account = resp.Account
+			default:
+				log.Panic().Err(fmt.Errorf("wrong response message")).Msg("❌ Could not get account information.")
+			}
+
+			signedTx, err := handler.SignTx(
+				unsignedTx, authsigning.SignerData{
+					ChainID:       handler.chainID,
+					AccountNumber: account.GetAccountNumber(),
+					Sequence:      account.GetSequence(),
+				},
+			)
+			if err != nil {
+				log.Panic().Err(err).Msg("❌ Could not sign transaction.")
+			}
+
+			tx, err := handler.EncodeTx(signedTx)
+			if err != nil {
+				log.Panic().Err(err).Msg("❌ Could not encode transaction.")
+			}
+
+			txResp, err := ctx.RequestFuture(
+				handler.cosmosClient,
+				&message.BroadcastTx{Deadline: msg.Deadline, Tx: tx},
+				time.Until(msg.Deadline),
+			).Result()
+			if err != nil {
+				log.Panic().Err(err).Msg("❌ Could not broadcast transaction.")
+			}
+
+			switch resp := txResp.(type) {
+			case *message.BroadcastTxResponse:
+				ctx.Send(msg.TxSubscriber, txResp)
+				if resp.TxResponse.Code != 0 {
+					log.Warn().
+						Int("messageCount", len(msg.Msgs)).
+						Interface("tx", resp.TxResponse).
+						Msg("😞 Transaction submitted with non 0 code")
+
+					if resp.TxResponse.Code == 32 && resp.TxResponse.Codespace == "sdk" {
+						log.Info().Int("attemptNum", attemptNum).Msg("account sequence mismatch, trying again")
+						attemptNum += 1
+					} else {
+						break
+					}
+				} else {
+					log.Info().
+						Int("messageCount", len(msg.Msgs)).
+						Str("txHash", resp.TxResponse.TxHash).
+						Uint32("txCode", resp.TxResponse.Code).
+						Msg("🚀 Successfully submit transaction")
+					break
+				}
+			default:
+				log.Panic().Err(fmt.Errorf("wrong response message")).Msg("❌ Could not broadcast transaction.")
+				break
+			}
 		}
 	}
 }
